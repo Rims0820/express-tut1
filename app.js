6b59fa222b7fed6cb196b6a49ff49df5337fc738
Srishti Singh <srishtirimmi20@gmail.com>
2025-10-24 12:28:46 +0530

    I have made changes in app.js,package-lock.json,package.json and made new files

--- app.js
+++ app.js
@@ -1,52 +1,24 @@
-const http = require('http')
-const {readFileSync} = require('fs');
+const express = require('express');
+const app = express()
+let {people} =require('./data')
 
-// get all files
-const homePage=readFileSync('./navbar-app/index.html')
-const homeStyles=readFileSync('./navbar-app/styles.css')
-const homeImage=readFileSync('./navbar-app/logo.svg')
-const homeLogic=readFileSync('./navbar-app/browser-app.js')
+// static assets
+app.use(express.static('./methods-public'))
+// parse from data
+app.use(express.urlencoded({extended:false}))
 
-const server=http.createServer((req,res)=>{
-    // console.log(req.method)
-    const url=req.url;
-    console.log(url);
-    // home page
-    if(url == '/'){
-
-    res.writeHead(200,{'content-type':'text/html'})
-    res.write(homePage)
-    res.end()
-  }
-//   about page
-  else if(url === '/about'){
-    res.writeHead(200,{'content-type':'text/html'})
-    res.write('<h1>about page</h1>')
-    res.end()
-  }
-//   styles
-  else if(url === '/styles.css'){
-    res.writeHead(200,{'content-type':'text/css'})
-    res.write(homeStyles)
-    res.end()
-  }
-//   logo
-  else if(url === '/logo.svg'){
-    res.writeHead(200,{'content-type':'image/svg+xml'})
-    res.write(homeImage)
-    res.end()
-  }
-//  logic 
-  else if(url === '/browser-app.js'){
-    res.writeHead(200,{'content-type':'text/javascript'})
-    res.write(homeLogic)
-    res.end()
-  }
-//   404
-  else{
-    res.writeHead(404,{'content-type':'text/html'})
-    res.write('<h1>page not found</h1>')
-    res.end()
-  }
+app.get('/api/people',(req,res)=>{
+  res.status(200).json({success:true,data:people})
 })
-server.listen(5000,()=> console.log('server running at http://localhost:5000'))
+
+app.post('/login',(req,res)=>{
+  const {name} = req.body;
+  if(name){
+    return res.status(200).send(`Welcome ${name}`)
+  }
+  res.send(401).send('Please provide credentials')
+})
+
+app.listen(5000,()=>{
+  console.log('server is listening on port 5000...')
+})
