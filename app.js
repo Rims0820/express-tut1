f296d5ca6ac57ae08428fd6b21e7005141de1961
Srishti Singh <srishtirimmi20@gmail.com>
2025-10-25 08:16:34 +0530

    Ihave made changes in app.js and created some new files

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const app = express()
-let {people} =require('./data')
-
+const people = require('./routes/people')
+const auth = require('./routes/auth')
 // static assets
 app.use(express.static('./methods-public'))
+
 // parse from data
 app.use(express.urlencoded({extended:false}))
 
-app.get('/api/people',(req,res)=>{
-  res.status(200).json({success:true,data:people})
-})
+// parse json
+app.use(express.json())
 
-app.post('/login',(req,res)=>{
-  const {name} = req.body;
-  if(name){
-    return res.status(200).send(`Welcome ${name}`)
-  }
-  res.send(401).send('Please provide credentials')
-})
+app.use('/api/people',people)
+app.use('/login',auth)
 
 app.listen(5000,()=>{
   console.log('server is listening on port 5000...')
